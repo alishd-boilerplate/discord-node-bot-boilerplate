3f7396416dc185bb03233ab12a44079539ba17c2
Ali Shadman <alishadman955@gmail.com>
2022-10-22 03:05:16 +0200

    [Fix] Removed version

--- scripts/version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-PACKAGE_VERSION=$(cat ./package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
-echo ::set-output name=pkgversion::$PACKAGE_VERSION
